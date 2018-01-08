46652981902c9b6339451f477607136146e4f399
Anton Abyzov <anton.abyzov@gmail.com>
2018-01-08 15:57:03 +0300

    concurrent test

--- LoggingModule.Tests/LoggingModuleTests.cs
+++ LoggingModule.Tests/LoggingModuleTests.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Options;
 using StructureMap;
 using Xunit;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace LoggingModule.Tests
 {
@@ -127,6 +128,44 @@ namespace LoggingModule.Tests
             logger.Log(Severity.Warn, new Exception($"Simple warn error {guid3}"));
             logger.Log(Severity.Error, new Exception($"Simple error error {guid4}"));
             logger.Log(Severity.Critical, new Exception($"Simple critical error {guid5}"));
+
+            //Assert
+            Thread.Sleep(5000);
+            var lastLines = File.ReadLines(GetFullName(fileName, (DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)))
+                .TakeLast(10).ToArray(); //10 - because every log puts 2 lines into log file
+            Assert.True(lastLines[0].Contains(guid5.ToString()));
+            Assert.True(lastLines[2].Contains(guid4.ToString()));
+            Assert.True(lastLines[4].Contains(guid3.ToString()));
+            Assert.True(lastLines[6].Contains(guid2.ToString()));
+            Assert.True(lastLines[8].Contains(guid.ToString()));
+        }
+
+        [Fact]
+        public void ShouldConsiderPriority_ForAllMessageTypes_Concurrent()
+        {
+            //Arrange
+            var fileName = $"testlog{Guid.NewGuid()}";
+            var logDir = _logDir;
+            _fileLoggerProvider = new FileLoggerProvider(new OptionsManager<FileLoggerOptions>(new List<IConfigureOptions<FileLoggerOptions>>(){new ConfigureOptions<FileLoggerOptions>(
+                (x) =>
+                {
+                    x.FileName = fileName;
+                    x.LogDirectory = logDir;
+                })
+            }));
+            var logger = _fileLoggerProvider.CreateLogger();
+            var guid = Guid.NewGuid();
+            var guid2 = Guid.NewGuid();
+            var guid3 = Guid.NewGuid();
+            var guid4 = Guid.NewGuid();
+            var guid5 = Guid.NewGuid();
+
+            //Act
+            new Thread(() => logger.Log(Severity.Debug, new Exception($"Simple debug error {guid}"))).Start();
+            new Thread(() => logger.Log(Severity.Info, new Exception($"Simple info error {guid2}"))).Start();
+            new Thread(() => logger.Log(Severity.Error, new Exception($"Simple error error {guid4}"))).Start();
+            new Thread(() => logger.Log(Severity.Critical, new Exception($"Simple critical error {guid5}"))).Start();
+            new Thread(() => logger.Log(Severity.Warn, new Exception($"Simple warn error {guid3}"))).Start();
 
             //Assert
             Thread.Sleep(5000);
